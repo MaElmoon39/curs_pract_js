f1c4c69b0a84ff2d9abcd92e865f374adb828128
Maria Elsa Tortello Ditta <maria.tortelloditta2@gmail.com>
2022-06-11 09:35:39 -0500

    Encapsulando código en funciones, creando los inputs en el html

--- figuras.js
+++ figuras.js
@@ -2,35 +2,41 @@
 
 //Código del cuadrado
 console.group("Cuadrado"); //esto es para agrupar mensajes de consola por grupos. Este abre...
-const ladoCuadrado = 5;
-console.log(`Los lados del cuadrado miden: ${ladoCuadrado}cm`);
+// const ladoCuadrado = 5;
+// console.log(`Los lados del cuadrado miden: ${ladoCuadrado}cm`);
 
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log(`El perimetro del cuadrado es: ${perimetroCuadrado}cm`);
+function perimetroCuadrado(lado) {
+  return lado * 4;
+}
+//console.log(`El perimetro del cuadrado es: ${perimetroCuadrado}cm`);
 
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log(`El área del cuadrado es: ${areaCuadrado}cm^2`);
-console.groupEnd(); //Y este cierra
+function areaCuadrado(lado) {
+  return lado * lado;
+}
+// console.log(`El área del cuadrado es: ${areaCuadrado}cm^2`);
+console.groupEnd(); //...Y este cierra
 
 //Código del triángulo
 console.group("Triángulo");
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-console.log(
-  `Los lados del triángulo miden: ${ladoTriangulo1}cm ${ladoTriangulo2}cm y ${baseTriangulo}cm`
-);
+// const ladoTriangulo1 = 6;
+// const ladoTriangulo2 = 6;
+// const baseTriangulo = 4;
+// console.log(
+//   `Los lados del triángulo miden: ${ladoTriangulo1}cm ${ladoTriangulo2}cm y ${baseTriangulo}cm`
+// );
 
-const alturaTriangulo = 5.5;
-console.log(
-  `La altura del triángulo es de: ${alturaTriangulo}cm`
-);
+// const alturaTriangulo = 5.5;
+// console.log(`La altura del triángulo es de: ${alturaTriangulo}cm`);
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-console.log(`El perimetro del triángulo es: ${perimetroTriangulo}cm`);
+function perimetroTriangulo(lado1, lado2, base) {
+  return lado1 + lado2 + base;
+}
+// console.log(`El perimetro del triángulo es: ${perimetroTriangulo}cm`);
 
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
-console.log(`El área del triángulo es: ${areaTriangulo}cm^2`)
+function areaTriangulo(base, altura) {
+  return (base * altura) / 2;
+}
+// console.log(`El área del triángulo es: ${areaTriangulo}cm^2`);
 
 console.groupEnd();
 
@@ -38,23 +44,50 @@ console.groupEnd();
 console.group("Círculos");
 
 //Radio
-const radioCirculo = 4;
-console.log(`El radio del círculo es: ${radioCirculo}cm`);
+// const radioCirculo = 4;
+// console.log(`El radio del círculo es: ${radioCirculo}cm`);
 
 //Diametro
-const diametroCirculo = radioCirculo * 2;
-console.log(`El diámetro del círculo es: ${diametroCirculo}cm`);
+// const diametroCirculo = radioCirculo * 2;
+function diametroCirculo(radio) {
+  return radio * 2;
+}
+// console.log(`El diámetro del círculo es: ${diametroCirculo}cm`);
 
 //Pi
-const PI = Math.PI
-console.log(`Pi es ${PI}`);
+const PI = Math.PI;
+// console.log(`Pi es ${PI}`);
 
 //Circunferencia
-const perimetroCircunferencia = diametroCirculo * PI;
-console.log(`El perímetro del círculo es: ${perimetroCircunferencia}cm`);
+// const perimetroCircunferencia = diametroCirculo * PI;
+function perimetroCircunferencia(radio) {
+  const diametro = diametroCirculo(radio);
+  return diametro * PI;
+}
+// console.log(`El perímetro del círculo es: ${perimetroCircunferencia}cm`);
 
 //Area
-const areaCirculo = (radioCirculo * radioCirculo) * PI;
-console.log(`El área del círculo es: ${areaCirculo}cm^2`);
+// const areaCirculo = (radioCirculo * radioCirculo) * PI;
+function areaCirculo(radio) {
+  return radio * radio * PI;
+}
+// console.log(`El área del círculo es: ${areaCirculo}cm^2`);
 
 console.groupEnd();
+
+//Aquí interactuamos con el HTML
+function calcularPerimetroCuadrado() {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const perimetro = perimetroCuadrado(value);
+  alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const area = areaCuadrado(value);
+  alert(area);
+}
